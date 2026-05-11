6b95f70b1356de8cc4fadf0f18eaf3f30a6cf40d
David <dwhenejr@gmail.com>
2026-05-11 19:27:14 -0400

    Added functions for each check

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import getpass
 from logo import banner
 print(banner)
 
-print("Welcome to the password strength test!")
+print("Welcome to the password strength test!\n")
 print("After entering your password, you will be given a score from 0-10. 10 being very secure and 0 being not secure at all. If the score is negative, you should change your password immediately.")
 print("This is purely for fun and educational purposes, this is not advice nor does it hold me liable for any passwords cracked after testing. NO password is 100% secure, please stay up to date on security trends and take your own precautions!!!\n")
 
@@ -12,46 +12,60 @@ with open('passlist.txt', 'r') as f:
 
 password_list = raw_text.splitlines()
 
-usr_pwd = getpass.getpass("Enter your password to be checked! ")
+usr_pwd = getpass.getpass("Enter your password to be checked! It will not show anything for security purposes. ")
 length = len(usr_pwd)
 
-safe = 16
 score = 0
 
-if length >= safe:
-    print("This password meets the length requirements.")
-    score += 3
-elif length < 16 and length > 10:
-    print("This password is good, but should be 16 characters.")
-    score +=1
-else:
-    print("This passwork is too short, it could be guessed or cracked more easily.")
-
-if usr_pwd in password_list:
-    print("Your password was found a the list of the 10000 most common passwords.")
-    score -= 1
-else:
-    print("Your password wasn't found in the 10,000 most common passwords.")
-    score += 1
-
-s = ""
-repeating = 0
-for letter in usr_pwd:
-    if s == letter:
-        repeating += 1
-        s = letter
+def length_test(usr_pwd):
+    global score
+    safe = 16
+    if length >= safe:
+        print("This password meets the length requirements.")
+        score += 3
+    elif length < 16 and length > 10:
+        print("This password is good, but should be 16 characters.")
+        score +=1
     else:
-        s = letter
+        print("This password is too short, it could be guessed or cracked more easily.")
 
-if repeating >= 3:
-    print("You have multiple repeating characters next to one another. This is bad security practice.")
-    score -= 1
-elif repeating >= 1:
-    print("You have repeating characters next to one another. This is bad security practice.")
-else:
-    print("Your password doesn't have any repeating characters, that is great!")
-    score += 1
+length_test(usr_pwd)
 
+def common_pwd_check(usr_pwd):
+    global score
+    if usr_pwd in password_list:
+        print("Your password was found a the list of the 10000 most common passwords.")
+        score -= 3
+    else:
+        print("Your password wasn't found in the 10,000 most common passwords.")
+        score += 1
+
+common_pwd_check(usr_pwd)
+
+def repeating_characters(usr_pwd):
+    global score
+    s = ""
+    repeating = 0
+    for letter in usr_pwd:
+        if s == letter:
+            repeating += 1
+            s = letter
+        else:
+            s = letter
+
+    if repeating >= 10:
+        print("You have ten or more repeating characters. This isn't bad, but it isn't good either. Try using a combination of different ones for length requirements, not just a spamming of the same one\n")
+        score += 1
+    if repeating >= 3 and repeating < 10:
+        print("You have multiple repeating characters next to one another. This is bad security practice.")
+        score -= 1
+    elif repeating >= 1 and repeating < 3:
+        print("You have repeating characters next to one another. This is bad security practice.")
+    elif repeating < 1:
+        print("Your password doesn't have any repeating characters, that is great!")
+        score += 2
+
+repeating_characters(usr_pwd)
 
 def variation(usr_pwd):
     global score
@@ -79,4 +93,4 @@ def variation(usr_pwd):
     
 variation(usr_pwd)
 
-print(score)
+print(f"Your password score is {score}")
